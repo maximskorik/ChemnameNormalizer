788b1fec427409199a831aed16e8d08691579bc0
maximskorik <maximskoryk@gmail.com>
2022-04-09 18:26:31 +0200

    add Normalizer interface

--- CompoundsNormalizer/normalization.py
+++ CompoundsNormalizer/normalization.py
@@ -1,10 +1,30 @@
+from aiohttp import ClientSession
+from CompoundsNormalizer import PubChemAPI
 from typing import List
 
 
 class Normalizer:
 
-    def __init__(self, normalization_format: str = "iupac"):
-        self._format = normalization_format
+    def __init__(self):
+        self.original_names = None
+        self.norm_names = None
 
-    def normalize(self, *args) -> List[str]:
+    async def normalize(self, names: List, normalization_format: str) -> List[str]:
+        self.original_names = names
+        await self._run_query(names, normalization_format)
+        self.norm_names = self.format_names(self.norm_names)
+        return self.norm_names
+
+    def read_names_from_file(self, file_path: str) -> List[str]:
         pass
+
+    async def _run_query(self, names: List, normalization_format: str) -> List[str]:
+        async with ClientSession() as session:
+            api = PubChemAPI(session=session, normalization_format=normalization_format)
+            norm_names = [await api.get_name(name) for name in names]
+        self.norm_names = norm_names
+
+    @staticmethod
+    def format_names(names: List) -> List[str]:
+        names = [name.lower().strip() if type(name) is str else name for name in names]
+        return names
--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -1,6 +1,16 @@
+import asyncio
 import pytest
+from CompoundsNormalizer import Normalizer
 
 
-@pytest.mark.skip(reason="Work in progress")
-def test_normalize():
-    pass
+@pytest.mark.asyncio
+@pytest.mark.parametrize("names, expected",
+     [
+         (["Adenosine", "Adenocard", "BG8967", "NONEXISTENT", "BAYT006267", "diflucan", "ibrutinib", "PC-32765"],
+          ["adenosine", "adenosine", "bivalirudin", None, "fluconazole", "fluconazole", "ibrutinib", "ibrutinib"])
+     ])
+async def test_normalize(names, expected):
+    app = Normalizer()
+    result = await app.normalize(names, "trivial")
+
+    assert result == expected
